532841904a8c5c4b15a13402597cdbd12e326dc4
Aditya Chavan <adityakchavan23@gmail.com>
2025-02-03 17:55:53 +0530

    objects part 2

--- 10_objects.js
+++ 10_objects.js
@@ -47,3 +47,66 @@ User.greetingTwo = function() {
     console.log(`Hello Users, ${this.name}`); // reference to the same object we write "this"
 }
 console.log(User.greetingTwo()); // Output:- Hello Users, Roman
+
+// Objects Constructor (Singleton)
+
+// Difference between the declaration (**Interview question**)
+const tinderUser = new Object() // singleton object 
+console.log(tinderUser); // Output:- {}
+
+const tinderUser0 = {} // non-singleton object
+console.log(tinderUser0); // Output:- {}
+
+const tinderUser1 = {} 
+tinderUser1.id = "567pqr"
+tinderUser1.name = "Sergey"
+tinderUser1.isLoggedIn = false
+console.log(tinderUser1); // Output:- { id: '567pqr', name: 'Sergey', isLoggedIn: false }
+
+// Object inside object
+const regularUser = {
+    email: "sergey@gmail.com",
+    fullName: { // object
+        userFullName: { // object
+            firstName: "Sergey",
+            lastName: "Prior"
+        }
+    }
+}
+console.log(regularUser.fullName); // Output:- { userFullName: { firstName: 'Sergey', lastName: 'Prior' } }
+console.log(regularUser.fullName.userFullName); // Output:- { firstName: 'Sergey', lastName: 'Prior' }
+console.log(regularUser.fullName.userFullName.firstName); // Output:- Sergey
+
+// Merge two or more objects together
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {obj1, obj2}
+console.log(obj3); // Output:- { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+const obj4 = Object.assign({}, obj1, obj2)
+console.log(obj4); // Output:- { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+const obj5 = {...obj1, ...obj2} // preferred way
+console.log(obj5); // Output:- { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+// Objects inside array (basic, when values come from the database)
+const users = [
+    {
+        id: 5,
+        email: "id5@gmail.com"
+    },
+    {
+        id: 6,
+        email: "id6@gmail.com"
+    },
+    {
+        id: 7,
+        email: "id7@gmail.com"
+    }
+]
+// users[5].email
+
+console.log(tinderUser1); // Output:- { id: '567pqr', name: 'Sergey', isLoggedIn: false }
+console.log(Object.keys(tinderUser1)); // Output:- [ 'id', 'name', 'isLoggedIn' ] (the output values datatype is array)
+console.log(Object.values(tinderUser1)); // Output:- [ '567pqr', 'Sergey', false ]
+console.log(Object.entries(tinderUser1)); // Output:- [ [ 'id', '567pqr' ], [ 'name', 'Sergey' ], [ 'isLoggedIn', false ] ]
+console.log(tinderUser1.hasOwnProperty('isLoggedIn')); // Output:- true
+console.log(tinderUser1.hasOwnProperty('isLogged')); // Output:- false
